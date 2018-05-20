84dfbce662f91d850cc50b59c3d911ffba88f4e7
Paul Dagnelie <paulcd2000@gmail.com>
2018-05-20 11:45:03 -0700

    add notes, update gitignore

--- login.php
+++ login.php
@@ -7,11 +7,12 @@
  * @param bool $svg Whether to show svg icons in the register and resendpwd links or not
  */
 function html_login($svg = false){
+    global $ID
     require('lib/plugins/discourse-sso/config.php');
     $payload_plain = "nonce=" . gen_nonce();
     $payload = base64_encode($payload_plain);
     $sig = hash_hmac("sha256", $payload_plain, $cfg_sso_secret, false);
-    // Store nonce
+    // Store nonce with $ID as redirect target; maybe done in gen_nonce?
     print p_locale_xhtml('login');
     print '<div class="centeralign">'.NL;
     print '<a href="' . $cfg_sso_url . '/sso?sso=' . $payload . '&sig=' . $sig . '>Log In with SSO</a>'
